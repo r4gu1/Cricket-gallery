56206c1b840ae03258f5fc7299db68cdd62add4a
r4gu1 <ragulramadoss17@gmail.com>
2024-09-14 15:43:20 +0530

    updated

---
+++ src/Components/Players.js
@@ -0,0 +1,82 @@
+import one from "../asseest/images/m1.jpeg"
+import two from "../asseest/images/m2.jpeg"
+import three from "../asseest/images/m3.jpeg"
+import four from "../asseest/images/m4.jpeg"
+import five from "../asseest/images/m5.jpeg"
+import six from "../asseest/images/m6.jpeg"
+import seven from "../asseest/images/m7.jpeg"
+import eight from "../asseest/images/m8.jpeg"
+import nine from "../asseest/images/m9.jpeg"
+import ten from "../asseest/images/m10.jpeg"
+import eleven from "../asseest/images/m11.jpeg"
+import twevel from "../asseest/images/m12.jpeg"
+
+function Player(){
+    return( <div class="box">
+
+
+            <div class="box1">
+             <img src={three}></img>
+             <h1>M S Dhoni</h1>
+            </div>
+            
+           <div class="box1">
+            <img src={one}></img>
+            <h1>Virat Kolhi</h1>
+        </div>
+
+        <div class="box1">
+            <img src={two}></img>
+            <h1>Sachin Tendulkar</h1>
+        </div>
+        
+        <div class="box1">
+            <img src={four}></img>
+            <h1> Gambheer</h1>
+        </div>
+
+        <div class="box1">
+            <img src={five}></img>
+            <h1>Ricky Ponting</h1>
+        </div>
+
+        <div class="box1">
+            <img src={six}></img>
+            <h1>Steve Smith</h1>
+        </div>
+
+        <div class="box1">
+            <img src={eleven}></img>
+            <h1>Babar Azam</h1>
+        </div>
+
+        <div class="box1">
+            <img src={seven}></img>
+            <h1>Rohit Sharma</h1>
+        </div>
+
+        <div class="box1">
+            <img src={eight}></img>
+            <h1>AB de Villiers</h1>
+        </div>
+
+        <div class="box1">
+            <img src={nine}></img>
+            <h1>Chris Gayle </h1>
+        </div>
+        
+        <div class="box1">
+            <img src={ten}></img>
+            <h1>Shane Watson</h1>
+        </div>
+       
+        <div class="box1">
+            <img src={twevel}></img>
+            <h1>Hardik Pandya</h1>
+        </div>
+
+        </div>
+    )
+}
+
+export default Player
